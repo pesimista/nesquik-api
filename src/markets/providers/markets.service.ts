27f80de393782a3a1b0c8c1226bc9b013137c471
Gary Nadir <nadirgary@gmail.com>
2022-05-30 16:22:27 -0400

    run linter

--- src/markets/providers/markets.service.ts
+++ src/markets/providers/markets.service.ts
@@ -1,10 +1,4 @@
-import {
-  ConflictException,
-  HttpException,
-  HttpStatus,
-  Inject,
-  Injectable,
-} from '@nestjs/common'
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common'
 import { InjectModel } from '@nestjs/mongoose'
 import { Model } from 'mongoose'
 import { Schedule } from 'nesquik-types'
